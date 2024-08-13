032082a3f0c085525ba235ea96de83a20bdf3873
KRath87 <k.rath@mbd-team.de>
2024-08-13 08:57:14 +0200

    kp was geändert ist.

--- Neustart.js
+++ Neustart.js
@@ -1,51 +1,51 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 let Begrüßung = prompt(
-  "Willkommen Held. In dieser Welt bewegst du dich mit den Tasten W, A, S und D."
+	"Willkommen Held. In dieser Welt bewegst du dich mit den Tasten W, A, S und D."
 );
 
 let map = [
-  ["▢", "▢", "▢"],
-  ["▢", "▢", "▢"],
-  ["▢", "▢", "▢"],
+	["▢", "▢", "▢"],
+	["▢", "▢", "▢"],
+	["▢", "▢", "▢"],
 ];
 
 const held = { x: 1, y: 1 };
 const offset = { x: 0, y: 0 };
 
 for (let health = 5000; health >= 0; ) {
-  for (let i = 0; i < map.length; i++) {
-    let row = "";
-    for (let j = 0; j < map[i].length; j++) {
-      if (i === held.y && j === held.x) {
-        row += "߷";
-      } else {
-        row += "▢";
-      }
-    }
+	for (let i = 0; i < map.length; i++) {
+		let row = "";
+		for (let j = 0; j < map[i].length; j++) {
+			if (i === held.y && j === held.x) {
+				row += "߷";
+			} else {
+				row += "▢";
+			}
+		}
 
-    console.log(row);
-  }
+		console.log(row);
+	}
 
-  //   if (held.x > offset.x + map[i][j] || held.y > offset.y + map.length) {
-  //     map.push("1");
-  //   }
-  move();
+	//   if (held.x > offset.x + map[i][j] || held.y > offset.y + map.length) {
+	//     map.push("1");
+	//   }
+	move();
 }
 
 function move() {
-  let dec1 = prompt("Wohin willst du gehen? ");
-  if (dec1.toLowerCase() === "w") {
-    held.y -= 1;
-  } else if (dec1.toLowerCase() === "s") {
-    held.y += 1;
-  } else if (dec1.toLowerCase() === "a") {
-    held.x -= 1;
-  } else if (dec1.toLowerCase() === "d") {
-    held.x += 1;
-  } else {
-    move();
-  }
+	let dec1 = prompt("Wohin willst du gehen? ");
+	if (dec1.toLowerCase() === "w") {
+		held.y -= 1;
+	} else if (dec1.toLowerCase() === "s") {
+		held.y += 1;
+	} else if (dec1.toLowerCase() === "a") {
+		held.x -= 1;
+	} else if (dec1.toLowerCase() === "d") {
+		held.x += 1;
+	} else {
+		move();
+	}
 }
 
 // function expansion() {
